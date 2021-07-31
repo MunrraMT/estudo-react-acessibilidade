e82e7ab5c24f3ce5ff6b0cf76f9c70b03cf4774f
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-31 13:52:06 -0400

    Change icon for image/svg

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,12 @@ import StyledHeader from './styles';
 const Header = () => (
   <StyledHeader>
     <StyledHeader.SocialMedia href='https://www.linkedin.com/in/andr%C3%A9-rodrigues-86369544/'>
-      <i className='fab fa-linkedin' />
+      <img
+        width='50'
+        height='50'
+        src='/fontawesome/linkedin-brands.svg'
+        alt='Logo do linkedin'
+      />
     </StyledHeader.SocialMedia>
     <StyledHeader.Title>
       Estudo de acessibilidade com ReactJS usando Wai-Aria
@@ -12,7 +17,12 @@ const Header = () => (
       Desenvolvido por <a href='https://github.com/MunrraMT'>André</a>
     </StyledHeader.SubTitle>
     <StyledHeader.SocialMedia href='https://github.com/MunrraMT'>
-      <i className='fab fa-github-square' />
+      <img
+        width='50'
+        height='50'
+        src='/fontawesome/github-square-brands.svg'
+        alt='Logo do github'
+      />
     </StyledHeader.SocialMedia>
   </StyledHeader>
 );
